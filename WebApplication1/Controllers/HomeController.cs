1926983f8c8823993fab6479f27b399facb0eacb
sayan <sayan@DESKTOP>
2021-06-03 23:47:54 +0530

    database connection string implemented

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using WebApplication1.Models;
 
@@ -12,14 +15,20 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
         {
+            string connectionstring = _config.GetConnectionString("TestDB");
+
+            ViewBag.Data = getdata();
             return View();
         }
 
@@ -32,6 +41,33 @@ namespace WebApplication1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public string getdata()
+        {
+
+            string tmp = string.Empty;
+            try
+            {
+                var strCon = _config.GetConnectionString("TestDB");
+                using (SqlConnection mConnection = new SqlConnection(strCon))
+                {
+                    using (SqlCommand command = new SqlCommand("RST_Test", mConnection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        //command.Parameters.AddWithValue("@UserId", UserId);
+                        //command.Parameters.AddWithValue("@FromDate", FromDate);
+                        //command.Parameters.AddWithValue("@ToDate", Todate);
+                        mConnection.Open();
+                        object scalarVal = command.ExecuteScalar();
+                        tmp = (scalarVal == null) ? "" : Convert.ToString(scalarVal);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               
+            }
+            return tmp;
         }
     }
 }
